5eaac973dbf2e246eeb0602cddddc788626978cf
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-23 15:30:59 -0600

    hooks up the search for an item and shortens the list to only display those items that the search query includes

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,33 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [query, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  function handleSearchChange(event)
+  {
+    setSearch(event.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  }).filter((item) => {
+    let myindex = -1;
+    if (query === undefined || query === null || query.length < 1) return true;
+    else myindex = item.name.indexOf(query);
+    if ((myindex > 0 || myindex == 0) && myindex < item.name.length) return true;//keep
+    else return false;//exclude
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} search={query} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
